0bc16ea112b44ac0eaddcb6960b0248bbcfdba6f
Tiago Santiago <87858900+Ti4goS@users.noreply.github.com>
2022-11-25 13:55:02 -0300

    att

--- barDoPrimo/MainWindow.xaml.cs
+++ barDoPrimo/MainWindow.xaml.cs
@@ -14,21 +14,21 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace barDoPrimo
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+namespace barDoPrimo;
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Process.Start("assemble.bat");
-        }
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        Process.Start("assemble.bat");
     }
 }
+
